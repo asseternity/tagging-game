9bbc17f0869e86d84d8b8761f007ffda4adf644d
asseternity <nakupovasset@gmail.com>
2024-12-09 12:17:49 +0400

    feat: popup window implementation

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './Popup.css';
 
-const Popup = ({ content }) => {
+const Popup = ({ children }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const togglePopup = () => {
     setIsVisible(!isVisible);
   };
 
+  const popupContent = (
+    <div className={`popup_content ${isVisible ? 'show' : ''}`}>
+      {children}
+      <button onClick={() => togglePopup()}>X</button>
+    </div>
+  );
+
   return (
     <div className="popup_container">
       <button onClick={() => togglePopup()}>Show popup!</button>
-      {isVisible && (
-        <div className={`popup_content ${isVisible ? 'show' : ''}`}>
-          {content}
-          <button onClick={() => togglePopup()}></button>
-        </div>
-      )}
+      {isVisible &&
+        ReactDOM.createPortal(popupContent, document.getElementById('portal'))}
     </div>
   );
 };
