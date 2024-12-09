8afef9310e8a54bb2d11b305e7a79b408495e7bc
asseternity <nakupovasset@gmail.com>
2024-12-09 15:55:39 +0400

    feat: add coordinate, scroll markers and storage, init task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import map from '/map.png';
 import Popup from './Popup';
 
@@ -12,20 +12,90 @@ function App() {
   // once you get it wrong, game ends, and if your score is in the all time top 5, a popup shows up where you can write your username
   // a button on the side that brings up a popup with the top 5 all time scores
 
+  const sampleTask = {
+    text: 'Click on Trowulan',
+    coords: { x: 2036.4000244140625, y: 949.600036621093 },
+  };
+
+  const [scrollPosition, setScrollPosition] = useState({
+    scrollX: 0,
+    scrollY: 0,
+  });
+
+  const [currentTask, setCurrentTask] = useState(sampleTask);
+
+  const [markers, setMarkers] = useState([]);
+
+  const mapContainerRef = useRef(null);
+
+  const handleMapClick = (e) => {
+    const image = e.target;
+    // get the bounding box of the map
+    const rect = image.getBoundingClientRect();
+    // calculate coords
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    console.log({ x, y });
+    console.log(scrollPosition);
+    handleNewMarker({ x, y });
+  };
+
+  const handleMapScroll = () => {
+    if (mapContainerRef.current) {
+      const scrollX = mapContainerRef.current.scrollLeft;
+      const scrollY = mapContainerRef.current.scrollTop;
+      setScrollPosition({ scrollX, scrollY });
+    }
+  };
+
+  const handleNewMarker = (coords) => {
+    if (coords.x !== null && coords.y !== null) {
+      setMarkers((prevMarkers) => [
+        ...prevMarkers,
+        { x: coords.x, y: coords.y },
+      ]);
+    }
+  };
+
   return (
     <div className="main_container">
       <div className="top_container">
-        <h1>Leordis map quiz</h1>
+        <h3>Leordis map quiz</h3>
+        <h4>{currentTask.text}</h4>
       </div>
-      <div className="map_container">
-        <img src={map} />
+      <div
+        className="map_container"
+        ref={mapContainerRef}
+        onScroll={() => handleMapScroll()}
+      >
+        <img src={map} onClick={(e) => handleMapClick(e)} />
+        {markers.map((marker, index) => {
+          return (
+            <div
+              key={index}
+              className="marker"
+              style={{ top: `${marker.y}px`, left: `${marker.x}px` }}
+            ></div>
+          );
+        })}
+        <div
+          style={{
+            top: `${currentTask.coords.y}px`,
+            left: `${currentTask.coords.x}px`,
+            position: 'absolute',
+            width: '35px',
+            height: '35px',
+            borderRadius: '50%',
+            backgroundColor: 'yellow',
+          }}
+        ></div>
       </div>
       <div className="rules_container">
         <h3>Rules</h3>
         <p>
-          Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum.
+          Read the task displayed at the top of the page. Scroll and explore the
+          map of Leordis. Click on the location where you believe the task is
+          pointing to.
         </p>
       </div>
       <div className="bottom_container">
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,11 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>
 );
+
+// [v] click on the map coords functionality - regardless of scroll or device
+// [v] track the scroll of the map
+// [v] place a marker on the image at the click
+// [v] spawn a square on the clicked area - THIS WORKS BECAUSE MARKERS ARE ABSOLUTE AND  PARENT IS MAP_CONTAINER WHICH IS RELATIVE
+// [_] spawn a larger circle - the current task
+// [_] check if a set coord is INSIDE the small square (this will be served as a random task from the api later)
+// [_] animations of the popup broken
