a555668524d86512a818cde6c35431cfdf319146
asseternity <nakupovasset@gmail.com>
2024-12-14 16:32:55 +0400

    feat: compile production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import App2 from './App2.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App2 />
+    <HashRouter>
+      <App2 />
+    </HashRouter>
   </StrictMode>
 );
 
